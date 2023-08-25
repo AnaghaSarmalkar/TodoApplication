aab73b16930ee52da1c15277598ed312f3136517
Anagha Sarmalkar <anagha.sarmalkar@lvt.com>
2023-08-24 21:06:32 -0600

    Added data layer components for Todo and TodoList pages

--- views/TodoList/TodoList.dataLayer.js
+++ views/TodoList/TodoList.dataLayer.js
@@ -1,5 +1,6 @@
 import { gql, useQuery } from "@apollo/client";
 import React from "react";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
 import TodoList from "./TodoList";
 
 export const getLocationsSimple = gql`
@@ -12,7 +13,9 @@ export const getLocationsSimple = gql`
 `;
 
 export default function TodoListDataLayer() {
+  const insets = useSafeAreaInsets();
   const { loading, error, data } = useQuery(getLocationsSimple);
-
-  return <TodoList data={data} loading={loading} error={error} />;
+  return (
+    <TodoList data={data} loading={loading} error={error} insets={insets} />
+  );
 }
